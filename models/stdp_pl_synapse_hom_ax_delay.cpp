526014cece69534a7a909d5da797986ab1d904ee
sanjay270597@github.com <sanjay270597@gmail.com>
2022-03-31 10:35:46 +0200

    Working code

--- models/stdp_pl_synapse_hom_ax_delay.cpp
+++ models/stdp_pl_synapse_hom_ax_delay.cpp
@@ -78,7 +78,8 @@ STDPPLHomAxDelayCommonProperties::set_status( const DictionaryDatum& d, Connecto
   updateValue< double >( d, names::lambda, lambda_ );
   updateValue< double >( d, names::alpha, alpha_ );
   updateValue< double >( d, names::mu, mu_ );
-  updateValue< double >( d, names::axonal_delay, axonal_delay_ );
+  bool ax_updated = updateValue< double >( d, names::axonal_delay, axonal_delay_ );
+  
   if ( axonal_delay_ < 0. ) //TODO Check also upper bounday with delay
   {
 	throw BadProperty( "axonal_delay >= 0. required." );
